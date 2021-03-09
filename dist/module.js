320df4143f1362719583d4d1e3a40e90e82fdd98
sergeisporyshev <sergei.sporyshev@gmail.com>
2021-03-09 21:15:55 +0800

    [issue #56] finally build

--- dist/module.js
+++ dist/module.js
@@ -24694,7 +24694,6 @@ function () {
 
   K8sPage.prototype.__getPodsUsedMemory = function () {
     var podsUsedMemory = {
-      //expr: 'sum(container_memory_usage_bytes{container_name!="", container_name!="POD"}) by (pod_name)'
       expr: 'sum(container_memory_working_set_bytes{container!="", container!="POD"}) by (pod) or ' + 'sum(label_replace(container_memory_working_set_bytes{container_name!="", container_name!="POD"}, "pod", "$1", "pod_name", "(.*)")) by (pod)',
       legend: 'pod'
     };
